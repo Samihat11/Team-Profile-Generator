89c6014491cb5ae53c967d4df303ac27c87bf01f
Samihat <sameerah11@hotmail.com>
2021-12-13 15:32:14 -0500

    added code to write to file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const Engineer = require('./lib/engineer.js');
 const Manager = require('./lib/manager.js');
 const Intern = require('./lib/intern');
+const fs = require('fs');
 
 const team = {
   name: '',
@@ -16,21 +16,25 @@ const questions = [
     message: 'What is the name of your team?',
     default: 'My team',
   },
-  { type: 'input', name: 'managerName', message: "Who is the manager of your team" },
+  {
+    type: 'input',
+    name: 'name',
+    message: 'Who is the manager of your team',
+  },
   {
     type: 'number',
     name: 'id',
-    message: (answers) => ` What is ${answers.managerName}'s employee ID`,
+    message: (answers) => ` What is ${answers.name}'s employee ID`,
   },
   {
     type: 'input',
     name: 'email',
-    message: (answers) => ` What is ${answers.managerName}'s email`,
+    message: (answers) => ` What is ${answers.name}'s email`,
   },
   {
     type: 'input',
     name: 'officeNum',
-    message: (answers) => ` What is ${answers.managerName}'s office number`,
+    message: (answers) => ` What is ${answers.name}'s office number`,
   },
 ];
 const questionsTwo = [
@@ -78,40 +82,110 @@ const addToTeam = async () => {
   });
   if (response.addTeam) {
     addEmployee();
-  }
-  else{
-    console.log(team)
+  } else {
+    createEmployeeCard(team)
   }
 };
 
 async function addEmployee() {
   try {
     const answers = await inquirer.prompt(questionsTwo);
-    createTeamMember(answers)
+    createTeamMember(answers);
     addToTeam();
   } catch (err) {
     console.log(err);
   }
 }
 
-const createTeamMember = async (answers) => {
+const createTeamMember = (answers) => {
   let employee;
-  const {teamMember, name, id , email, school, github} = answers
-  if(teamMember === 'Engineer'){
-    employee = new Engineer(name, id, email, github)
+  const { teamMember, name, id, email, school, github } = answers;
+  if (teamMember === 'Engineer') {
+    employee = new Engineer(name, id, email, github);
+  } else {
+    employee = new Intern(name, id, email, school);
+  }
+  team.employees.push(employee);
+};
+
+const createEmployeeCard = (team) => { 
+  let employeeCard;
+team.employees.forEach((employee) => {
+   employeeCard = `
+ <section class="has-background-grey-lighter mb-4 p-4">
+   <h2 class="card-header-subtitle mb-2 is-size-5">${employee.getName()}</h2>
+   <h3 class="card-header-subtitle">${employee.getRole()}</h3>
+ </section>
+ <section class="px-4 pb-4">
+   <ul class="mb-4" id="info">
+     <li class="mb-2">Employee ID: ${employee.getId()}</li>
+     <li class="mb-2">Email: ${employee.getEmail()}</li>
+     <li class="mb-2">${getInfo(employee)}</li>
+   </ul>
+ </section>
+`;
+ });
+createHtml(employeeCard)
+}
+
+const getInfo = (employee) => {
+  const role = employee.getRole();
+  if(role === 'Manager') {
+    return `<li>Office Number: ${employee.getOfficeNum()}</li>`
+  }
+  else if(role === 'Engineer') {
+    return `<li>Github: ${employee.getGithub()}</li>`
   }
   else{
-    employee = new Intern(name, id, email, school)
+    return `<li>School: ${employee.getSchool()}</li>`
   }
-  team.employees.push(employee)
+}
+
+const createHtml = (employeeCard) => {
+  let output = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css"
+    />
+    <title>${team.name}</title>
+  </head>
+  <body>
+    <header class="hero is-primary mb-4">
+      <h1 class="title is-2 has-text-dark has-text-centered p-2">Test</h1>
+    </header>
+    <main
+      class="
+        container
+        is-flex is-justify-content-center is-flex-wrap-wrap is-fluid
+      "
+    >  
+    <section class="card m-4">
+    ${employeeCard}
+    </section>
+    </main>
+  </body>
+</html>`
+fs.writeFile('./dist/index.html', output, (err) => {
+  if (err) {
+    console.log('There was an error creating your HTML file.');
+    console.error(err);
+    return;
+  }
+  console.log(`Your file was successfully created.`);
+});
 }
 
 async function init() {
   try {
     const answers = await inquirer.prompt(questions);
-    const {teamName, name, id, email, officeNum} = answers
+    const { teamName, name, id, email, officeNum } = answers;
     team.name = teamName;
-    team.employees.push(new Manager(id, name, email, officeNum))
+    team.employees.push(new Manager(id, name, email, officeNum));
     addToTeam();
   } catch (err) {
     console.log(err);
@@ -122,6 +196,5 @@ init();
 
 /* 
  Validate user input
- Write to file
  Write test
 */
